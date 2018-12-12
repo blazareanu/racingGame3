8da207e6b2e7bf3c35e49ee502d06f0b3ebb7e17
blazareanu <lazareanubianca@gmail.com>
2018-12-12 21:14:51 +0200

    studied classes, variables and premitive data types

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,37 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //declaring a variable and instantiating a Car object;
+        //this is also called initialising a variable;
+
+      Car firstCompetitor = new Car ();
+    firstCompetitor.name = "Dacia";
+    firstCompetitor.color= "black";
+    firstCompetitor.doorCount= 5;
+    firstCompetitor.mileage = 0.5;
+
+    // local variables are variables declared inside a method;
+        // they don't receive default values;
+
+        Car secondCompetitor = new Car();
+        secondCompetitor.name ="Golf";
+
+  Engine firstEngine = new Engine ();
+  firstEngine.manufacturer= "Renault";
+
+  //sout prints and message to the console;
+
+        System.out.println(firstEngine.manufacturer);
+    firstCompetitor.engine = firstEngine;
+
+firstCompetitor.engine.manufacturer ="BMW";
+
+firstCompetitor = null;
+
+        System.out.println(firstEngine.manufacturer);
+        System.out.println(firstCompetitor.engine.manufacturer);
+
+
+
     }
 }
